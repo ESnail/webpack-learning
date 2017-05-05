777384b143709876ee5281c4c46a8489495e65f0
rxsun <rxsun@iflytek.com>
2017-05-05 14:38:46 +0800

    webpack2-1-1

---
+++ webpack1-2-2/src/script/main.js
@@ -0,0 +1,5 @@
+function hello(str) {
+    console.log('str', str);
+}
+
+hello('Hello world!');
---
+++ webpack1-2-2/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: ['./src/script/main.js','./src/script/a.js'],
+    output: {
+        path: './dist/js',
+        filename: 'bundle.js'
+    }
+}
